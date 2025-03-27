a606f68f587606a0a982568bf043fd52afe1a502
Pranjalkumari <kumaripranjal694@gmail.com>
2025-03-27 23:35:54 +0530

    previous

--- learnReact/src/main.jsx
+++ learnReact/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
-
+const anotherEle = (
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
   
+      // we can aplso write like app()  { as it is a function }
+      <app/>
+    //  anotherEle 
 
 );
 
