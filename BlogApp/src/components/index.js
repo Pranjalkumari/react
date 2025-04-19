43e16c4e683941b1325d79065f2b53db9c34990f
Pranjalkumari <kumaripranjal694@gmail.com>
2025-04-19 23:12:40 +0530

    completed routing

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -2,5 +2,30 @@ import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
 import Container from './container/Container'
 import Logo from './Logo'
+import RTE from './RTE'
+import Logoutbtn from "./Header/Logoutbtn"
+import Login from "./Login"
+import  Signup from './Signup'
+import PostForm from "./Post_form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Button from "./Button"
+import Select from "./Select"
+export {
+    Header,
+     Footer, 
+     Container, 
+     Logo,
+    Logoutbtn,
+RTE,
+Login,
+Signup,
 
-export {Header, Footer, Container, Logo}
+PostForm,
+PostCard,
+Button,
+Select,
+AuthLayout,
+Input
+}
