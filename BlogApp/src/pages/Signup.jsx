b660a31216b9a6ab8e6943dee3ec2398a29c3bf0
Pranjalkumari <kumaripranjal694@gmail.com>
2025-04-21 12:41:22 +0530

    pages

--- BlogApp/src/pages/Signup.jsx
+++ BlogApp/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import {Signup as SignupComponent} from '../components/Signup'
+import  SignupComponent from '../components/Signup'
 function Signup() {
   return (
     <div className='py-8'>
